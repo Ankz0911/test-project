f30d711ca2a6546be41e3cc3715bd139f26f48d7
Ankz0911 <ankur.khurana.cool@gmail.com>
2022-06-06 20:37:09 -0400

    removed secret key from environ variables

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get(SECRET_KEY)
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+# app.config['SECRET_KEY'] = os.environ.get(SECRET_KEY)
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
