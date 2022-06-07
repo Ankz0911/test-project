e092e23602a4be9de83d0b6b8f3e195d64adfd44
Ankz0911 <ankur.khurana.cool@gmail.com>
2022-06-06 20:41:09 -0400

    upgrading to PostgreSQL

--- main.py
+++ main.py
@@ -17,11 +17,12 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(POSTGRE_URL)
 # database_url = os.environ.get(POSTGRE_URL)
 # if database_url != None:
 #     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(DATABASE_URL)
 # else:
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
